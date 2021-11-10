2895a13a6521b0d66b79e82cf9a25edf1372f270
FarakYahia <93945749+FarakYahia@users.noreply.github.com>
2021-11-10 08:29:21 -0800

    Add files via upload

---
+++ W01D03/main.js
@@ -0,0 +1,84 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+//Pulse Check
+//1 Figure out the syntax errors in the following functions, and fix them.
+//const myFunction = function parameter) {}
+const myFunction = function (parameter) {}
+//unction = function (parameter) {}
+const unction = function (parameter) {}
+//function (parameter) {}
+const unction2 = function (parameter) {}
+//e = function (paramOne paramTwo) {}
+const e = function (paramOne, paramTwo) {}
+
+//2 Write a function sayHello that returns Hello when executed.
+
+
+const sayHello = function () {
+    return 'Hello'
+}
+
+
+//3
+const myAge = function (age) {
+    return age
+}
+//4
+const incrementOne = function (number) {
+    return number+1
+}
+
+//5
+const greet = function (name) {
+      return 'Heloo'+name
+}
+
+//Practice
+//1
+const double = function (number) {
+    return number*2
+}
+
+//2
+const fullName = function (firstName, lastName) {
+return firstName+ lastName
+}
+
+//3
+const average = function  (a,b) {
+    return (a+b)/2
+}
+
+//4
+const square = function (number) {
+    return number * number;
+  };
+  const cube = function (number) {
+    return number * number * number;
+
+    //5
+    const totalBill = function (total, taxPercentage, tip) {
+        return total+ taxPercentage+ tip
+    }
+
+    //6
+    const ageInHours = function (ageInYears) {
+        return ageInYears = (23*365)*24
+    }  
+    //7
+    const calculateProfit = function (unitsSold, unitCost, unitCost) {
+       return unitsSold*(unitCost+unitCost)
+    }
+    //8
+    const floor = function (number) {
+    return Math.floor(number)
+    }
+//9
+const toThePowerOf = function (base, exponent) {
+return Math.pow(base, exponen)
+}
+//10
+const upperCase = function (string) {
+    return toUpperCase(string)
+}
